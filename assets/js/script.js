7d538d52fa5b264fd889944f8aa91c7a47671ee8
BobWritesCode <warwick_hart@hotmail.com>
2022-07-21 15:38:17 +0000

    Create function loadGenerator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log("connected");
+
+window.addEventListener('DOMContentLoaded', function() {
+  console.log("DOMContentLoaded");
+  document.getElementById("btn-load").addEventListener("click", function () {
+    loadGenerator();
+  });
+});
+
+function loadGenerator() {
+  const btnLoad = document.getElementById('btn-load');
+  btnLoad.style.display = "none";
+}
