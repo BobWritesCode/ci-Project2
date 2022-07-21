c527724f5e326648b5ab6f638cde287de7989e0f
BobWritesCode <warwick_hart@hotmail.com>
2022-07-21 16:52:47 +0000

    Make resultContainer go full screen when activated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 console.log("connected");
 
 const multipleWinsHTML = document.getElementById("multiple-wins");
+const resultContainerHTML = document.getElementById("result-container");
 
 window.addEventListener('DOMContentLoaded', function() {
   console.log("DOMContentLoaded");
@@ -12,7 +13,24 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function loadGenerator() {
   const btnLoad = document.getElementById('btn-load');
-  btnLoad.style.display = "none";
+  //btnLoad.style.display = "none";
+  resultContainerHTML.style.backgroundColor = "red";
+  
+  resultContainerHTML.style.position = "absolute";
+  resultContainerHTML.style.top = "0px";
+  resultContainerHTML.style.right = "0px";
+  resultContainerHTML.style.bottom = "0px";
+  resultContainerHTML.style.left = "0px";
+  
+  // Credit for fullscreen code: https://www.w3schools.com/howto/howto_js_fullscreen.asp
+  if (resultContainerHTML.requestFullscreen) {
+    resultContainerHTML.requestFullscreen();
+  } else if (resultContainerHTML.webkitRequestFullscreen) { /* Safari */
+  resultContainerHTML.webkitRequestFullscreen();
+  } else if (resultContainerHTML.msRequestFullscreen) { /* IE11 */
+  resultContainerHTML.msRequestFullscreen();
+  }
+
 }
 
 function sortOptions() {
