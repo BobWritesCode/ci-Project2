25e1bcab94fea4459c99cc2a6848de3dd9ce7420
BobWritesCode <warwick_hart@hotmail.com>
2022-07-30 17:59:25 +0000

    Improve how results are presented
    - Change results format from flex to grid
    
    - Make sure title and tail of results do not display while not in generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ const template1ContainerHTML = document.getElementById('template1-container');
 const template2ContainerHTML = document.getElementById('template2-container');
 const template3ContainerHTML = document.getElementById('template3-container');
 const results = [];
+const resultTop = document.getElementById('result-top');
+const resultBottom = document.getElementById('result-bottom');
+
 
 window.addEventListener('DOMContentLoaded', function() {
   btnLoad.addEventListener("click", function () {
@@ -59,8 +62,15 @@ function loadGenerator() {
   } else if (resultContainerHTML.msRequestFullscreen) { /* IE11 */
   resultContainerHTML.msRequestFullscreen();
   }
+  resultTop.style.display = "block";
+  resultBottom.style.display = "block";
   btnStart.style.display = "block";
   btnClose.style.display = "block";
+  const userGenTitle = document.getElementById('gen-title-user').value;
+  console.log(userGenTitle)
+  const spanGenTitle = document.getElementById('gen-title-span');
+  console.log(spanGenTitle)
+  spanGenTitle.innerText = userGenTitle
 }
 
 function exitGenerator() {
@@ -77,6 +87,8 @@ function exitGenerator() {
   resultContainerHTML.style.position = "initial";
   btnClose.style.display = "none";
   btnStart.style.display = "none";
+  resultTop.style.display = "none";
+  resultBottom.style.display = "none";
   btnLoad.style.display = "block";
 }
 
@@ -102,8 +114,8 @@ function createRandomResults(options, transitionTime, timeBetween) {
 }
 
 function show1By1(results, transitionTime, timeBetween) {
-  let vertOrHorValue = document.getElementById('vert-or-hor').value;
-  switch (vertOrHorValue) {
+  /*let vertOrHorValue = document.getElementById('vert-or-hor').value;
+   switch (vertOrHorValue) {
     case "vertical":
       resultsListHTML.style.flexDirection = "column";
       break;
@@ -111,14 +123,16 @@ function show1By1(results, transitionTime, timeBetween) {
       resultsListHTML.style.flexDirection = "row";
       break;
   }
+  */
   for (let i of results){
-    let newLi = document.createElement('li');
-    newLi.className = "box";
-    newLi.appendChild(document.createTextNode(i));
-    resultsListHTML.appendChild(newLi);
+    let newDiv = document.createElement('div');
+    newDiv.className = "box";
+    newDiv.appendChild(document.createTextNode(i));
+    resultsListHTML.appendChild(newDiv);
   }
+
   setTimeout(function() {
-    let elements = document.querySelectorAll("ul#results-list > li");
+    let elements = document.querySelectorAll("span#results-list > div");
     let delay = 0
     for (let element of elements) {
       element.style.transitionDuration  = `${transitionTime}s`
